4c7166aa905ead6c391a047417901501c5f61bff
Menkar85 <122032374+Menkar85@users.noreply.github.com>
2023-09-19 22:01:36 +1000

    Update pull_packer.sh
    folder check

--- nvim/pull_packer.sh
+++ nvim/pull_packer.sh
@@ -6,7 +6,7 @@ sudo apt install neovim -y
 sudo apt install cmake -y
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
- mkdir /home/${USER}/.config
+ [ -d /home/$USER/.config ] || mkdir /home/${USER}/.config
  cd
  cp -r ./nvim /home/${USER}/.config/ && rm /home/${USER}/.config/nvim/pull_packer.sh
  echo 'Completed'
